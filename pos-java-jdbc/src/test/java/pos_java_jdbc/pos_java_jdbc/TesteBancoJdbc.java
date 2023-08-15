c91376cb80c8fba3f10809897f6e518ea1e4197f
samuel <samugit.projects@gmail.com>
2023-08-15 17:54:14 -0300

    Aula21_10 Criando nosso INSERT ao banco de dados

--- pos-java-jdbc/src/test/java/pos_java_jdbc/pos_java_jdbc/TesteBancoJdbc.java
+++ pos-java-jdbc/src/test/java/pos_java_jdbc/pos_java_jdbc/TesteBancoJdbc.java
@@ -2,13 +2,22 @@ package pos_java_jdbc.pos_java_jdbc;
 
 import org.junit.Test;
 
-import conexaojdbc.SingleConnection;
+import dao.UserPosDAO;
+import model.Userposjava;
 
 public class TesteBancoJdbc {
 	
 	@Test
 	public void initBanco() {
-		SingleConnection.getConnection();
+		UserPosDAO userPosDAO = new UserPosDAO();
+		Userposjava userposjava = new Userposjava();
+		
+		userposjava.setId(6L);
+		userposjava.setNome("paulo");
+		userposjava.setEmail("paulo@gmail.com");
+		
+		userPosDAO.salvar(userposjava);
+		
 	}
 
 }
